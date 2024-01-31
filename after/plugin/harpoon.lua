07fa93d0526187c743bc27c8d107b1073d9b9f70
Ahmad Shabeer K <shabeer@sellowpay.com>
2024-01-31 12:42:08 +0530

    tab/space

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -4,7 +4,7 @@ harpoon:setup();
 
 vim.keymap.set("n", "<leader>a", function () harpoon:list():append(); end);
 vim.keymap.set("n", "<C-e>", function ()
-    harpoon.ui:toggle_quick_menu(harpoon:list());
+	harpoon.ui:toggle_quick_menu(harpoon:list());
 end);
 
 vim.keymap.set("n", "<A-h>", function () harpoon:list():select(1) end);
--- lua/ask/packer.lua
+++ lua/ask/packer.lua
@@ -4,62 +4,62 @@
 vim.cmd [[packadd packer.nvim]];
 
 return require('packer').startup(function (use)
-    -- Packer can manage itself
-    use('wbthomason/packer.nvim');
-    use('rstacruz/vim-closer');
-    use({
-        'nvim-telescope/telescope.nvim',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    });
-    use('nvim-tree/nvim-web-devicons');
-    use('themaxmarchuk/tailwindcss-colors.nvim');
-    use({ "catppuccin/nvim", as = "catppuccin" });
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' });
-    use('nvim-treesitter/playground');
-    use({
-        "ThePrimeagen/harpoon",
-        branch = "harpoon2",
-        requires = { { "nvim-lua/plenary.nvim" } }
-    });
-    use('tpope/vim-fugitive');
-    use('tpope/vim-commentary');
-    use('mbbill/undotree');
-    use('nvimtools/none-ls.nvim');
-    use('nvim-telescope/telescope-ui-select.nvim');
-    use({ 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' });
-    use('christoomey/vim-tmux-navigator');
-    use('lewis6991/gitsigns.nvim');
-    use('j-hui/fidget.nvim');
-    use('sindrets/diffview.nvim');
-    use('folke/zen-mode.nvim');
-    use('rafamadriz/friendly-snippets');
-    use({
-        "windwp/nvim-autopairs",
-        config = function () require("nvim-autopairs").setup {}; end,
-    });
-    use({
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' }, -- Required
-            {                            -- Optional
-                'williamboman/mason.nvim',
-                run = function ()
-                    pcall(vim.cmd, 'MasonUpdate');
-                end,
-            },
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+	-- Packer can manage itself
+	use('wbthomason/packer.nvim');
+	use('rstacruz/vim-closer');
+	use({
+		'nvim-telescope/telescope.nvim',
+		requires = { { 'nvim-lua/plenary.nvim' } }
+	});
+	use('nvim-tree/nvim-web-devicons');
+	use('themaxmarchuk/tailwindcss-colors.nvim');
+	use({ "catppuccin/nvim", as = "catppuccin" });
+	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' });
+	use('nvim-treesitter/playground');
+	use({
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		requires = { { "nvim-lua/plenary.nvim" } }
+	});
+	use('tpope/vim-fugitive');
+	use('tpope/vim-commentary');
+	use('mbbill/undotree');
+	use('nvimtools/none-ls.nvim');
+	use('nvim-telescope/telescope-ui-select.nvim');
+	use({ 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' });
+	use('christoomey/vim-tmux-navigator');
+	use('lewis6991/gitsigns.nvim');
+	use('j-hui/fidget.nvim');
+	use('sindrets/diffview.nvim');
+	use('folke/zen-mode.nvim');
+	use('rafamadriz/friendly-snippets');
+	use({
+		"windwp/nvim-autopairs",
+		config = function () require("nvim-autopairs").setup {}; end,
+	});
+	use({
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		requires = {
+			-- LSP Support
+			{ 'neovim/nvim-lspconfig' }, -- Required
+			{                   -- Optional
+				'williamboman/mason.nvim',
+				run = function ()
+					pcall(vim.cmd, 'MasonUpdate');
+				end,
+			},
+			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },     -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            {
-                'L3MON4D3/LuaSnip',
-                build = "make install_jsregexp"
-            }, -- Required
-            { 'hrsh7th/cmp-path' },
-            { 'hrsh7th/cmp-buffer' },
-        }
-    });
+			-- Autocompletion
+			{ 'hrsh7th/nvim-cmp' }, -- Required
+			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
+			{
+				'L3MON4D3/LuaSnip',
+				build = "make install_jsregexp"
+			}, -- Required
+			{ 'hrsh7th/cmp-path' },
+			{ 'hrsh7th/cmp-buffer' },
+		}
+	});
 end);
