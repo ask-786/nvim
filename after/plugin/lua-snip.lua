7474e4b4d22a6420c8c0182ef5f24fed1cb1b8bf
Ahmad Shabeer K <shabeer@sellowpay.com>
2024-02-26 12:44:25 +0530

    snippets

--- after/plugin/lua-snip.lua
+++ after/plugin/lua-snip.lua
@@ -1,4 +1,5 @@
 local ls = require('luasnip');
+local from_vscode = require('luasnip.loaders.from_vscode');
 
 vim.keymap.set({ 'i' }, '<C-K>', function () ls.expand() end, { silent = true })
 vim.keymap.set({ 'i', 's' }, '<C-L>', function () ls.jump(1) end,
@@ -13,7 +14,8 @@ vim.keymap.set({ 'i', 's' }, '<C-E>', function ()
 	end
 end, { silent = true })
 
-require('luasnip.loaders.from_vscode').lazy_load()
-require('luasnip.loaders.from_vscode').load_standalone({
+ls.filetype_extend('angular', { 'html' })
+from_vscode.lazy_load()
+from_vscode.load_standalone({
 	path = '.vscode/ocw-v35.code-snippets'
 })
