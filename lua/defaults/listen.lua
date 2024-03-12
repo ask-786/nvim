b26b75f7b66d1bce953c356144016904f0ea6341
Ahmad Shabeer K <shabeer@sellowpay.com>
2024-03-12 10:04:01 +0530

    bla

--- lua/defaults/listen.lua
+++ lua/defaults/listen.lua
@@ -10,7 +10,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
-	group = vim.api.nvim_create_augroup('remove-trailing-space', { clear = true }),
+	group = vim.api.nvim_create_augroup(
+		'remove-trailing-space',
+		{ clear = true }
+	),
 	pattern = '*',
 	command = [[%s/\s\+$//e]],
 })
