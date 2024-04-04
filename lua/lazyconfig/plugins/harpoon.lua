cc4d0df24e2901ad638febda71c80e5b208d3821
Ahmad Shabeer K <shabeer@sellowpay.com>
2024-04-04 10:48:07 +0530

    chore: harpoon update deprecation.

--- lua/lazyconfig/plugins/harpoon.lua
+++ lua/lazyconfig/plugins/harpoon.lua
@@ -4,8 +4,9 @@ local config = function()
 	harpoon:setup()
 
 	vim.keymap.set('n', '<leader>a', function()
-		harpoon:list():append()
+		harpoon:list():add()
 	end, { desc = '[A]dd file (harpoon)' })
+
 	vim.keymap.set('n', '<C-e>', function()
 		harpoon.ui:toggle_quick_menu(harpoon:list())
 	end, { desc = 'Toggle quickmenu (harpoon)' })
@@ -13,12 +14,15 @@ local config = function()
 	vim.keymap.set('n', '<A-h>', function()
 		harpoon:list():select(1)
 	end, { desc = 'File 1 (harpoon)' })
+
 	vim.keymap.set('n', '<A-j>', function()
 		harpoon:list():select(2)
 	end, { desc = 'File 2 (harpoon)' })
+
 	vim.keymap.set('n', '<A-k>', function()
 		harpoon:list():select(3)
 	end, { desc = 'File 2 (harpoon)' })
+
 	vim.keymap.set('n', '<A-l>', function()
 		harpoon:list():select(4)
 	end, { desc = 'File 4 (harpoon)' })
