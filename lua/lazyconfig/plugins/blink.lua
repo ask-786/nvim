0acc07f69e2de084d66639d5903241a1e93de692
Ahmad Shabeer K <ahmadshabeerk786@gmail.com>
2025-01-21 19:25:29 +0530

    updates

--- lua/lazyconfig/plugins/blink.lua
+++ lua/lazyconfig/plugins/blink.lua
@@ -1,55 +1,53 @@
-local get_opts = function()
-	---@module 'blink.cmp'
-	---@type blink.cmp.Config
-	return {
-		keymap = {
-			preset = 'none',
-			['<CR>'] = { 'select_and_accept', 'fallback' },
-			['<C-y>'] = { 'select_and_accept', 'fallback' },
-			['<C-e>'] = { 'hide', 'fallback' },
-			['<C-p>'] = { 'show', 'select_prev', 'fallback' },
-			['<C-n>'] = { 'show', 'select_next', 'fallback' },
-			['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
-			['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
-			['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+---@module 'blink.cmp'
+---@type blink.cmp.Config
+local opts = {
+	keymap = {
+		preset = 'none',
+		['<CR>'] = { 'select_and_accept', 'fallback' },
+		['<C-y>'] = { 'select_and_accept', 'fallback' },
+		['<C-e>'] = { 'hide', 'fallback' },
+		['<C-p>'] = { 'show', 'select_prev', 'fallback' },
+		['<C-n>'] = { 'show', 'select_next', 'fallback' },
+		['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
+		['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
+		['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+	},
+	appearance = { use_nvim_cmp_as_default = true, nerd_font_variant = 'mono' },
+	signature = { enabled = true, window = { border = 'rounded' } },
+	sources = {
+		default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer', 'dadbod' },
+		cmdline = {},
+		providers = {
+			dadbod = { name = 'Dadbod', module = 'vim_dadbod_completion.blink' },
+			lazydev = {
+				name = 'LazyDev',
+				module = 'lazydev.integrations.blink',
+				score_offset = 10,
+			},
+			lsp = { fallbacks = {}, score_offset = 5, async = true },
 		},
-		appearance = { use_nvim_cmp_as_default = true, nerd_font_variant = 'mono' },
-		signature = { enabled = true, window = { border = 'rounded' } },
-		sources = {
-			default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer', 'dadbod' },
-			cmdline = {},
-			providers = {
-				dadbod = { name = 'Dadbod', module = 'vim_dadbod_completion.blink' },
-				lazydev = {
-					name = 'LazyDev',
-					module = 'lazydev.integrations.blink',
-					score_offset = 10,
-				},
-				lsp = { fallbacks = {}, score_offset = 5 },
+	},
+	completion = {
+		menu = { auto_show = true, border = 'rounded' },
+		list = {
+			selection = {
+				preselect = false,
+				auto_insert = true,
 			},
 		},
-		completion = {
-			menu = { auto_show = true, border = 'rounded' },
-			list = {
-				selection = {
-					preselect = false,
-					auto_insert = true,
-				},
-			},
-			trigger = { show_on_insert_on_trigger_character = false },
-			documentation = {
-				auto_show = true,
-				auto_show_delay_ms = 200,
-				window = { border = 'rounded' },
-			},
+		trigger = { show_on_insert_on_trigger_character = false },
+		documentation = {
+			auto_show = true,
+			auto_show_delay_ms = 200,
+			window = { border = 'rounded' },
 		},
-		snippets = { preset = 'luasnip' },
-	}
-end
+	},
+	snippets = { preset = 'luasnip' },
+}
 
 return {
 	'saghen/blink.cmp',
 	dependencies = { 'L3MON4D3/LuaSnip' },
 	build = 'cargo build --release',
-	opts = get_opts,
+	opts = opts,
 }
--- lua/lazyconfig/util/lsp.lua
+++ lua/lazyconfig/util/lsp.lua
@@ -26,7 +26,7 @@ M.lsp_highlight = function(event)
 	end
 
 	local highlight_augroup =
-		vim.api.nvim_create_augroup('ask-lsp-highlight', { clear = false })
+		vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
 
 	vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
 		buffer = event.buf,
@@ -41,11 +41,11 @@ M.lsp_highlight = function(event)
 	})
 
 	vim.api.nvim_create_autocmd('LspDetach', {
-		group = vim.api.nvim_create_augroup('ask-lsp-detach', { clear = true }),
+		group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
 		callback = function(event2)
 			vim.lsp.buf.clear_references()
 			vim.api.nvim_clear_autocmds({
-				group = 'ask-lsp-highlight',
+				group = 'lsp-highlight',
 				buffer = event2.buf,
 			})
 		end,
