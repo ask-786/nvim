7e96e2a6815f0339eb27176ddfff199dbb501cac
Ahmad Shabeer K <ahmadshabeerk786@gmail.com>
2025-02-11 12:16:06 +0530

    json to ts

---
+++ lua/lazyconfig/plugins/json-to-ts.lua
@@ -0,0 +1,9 @@
+local map = require('lazyconfig.util').map_with_desc
+
+return {
+	'ask-786/json-to-ts.nvim',
+	config = function()
+		local json_to_ts = require('json-to-ts')
+		map('n', '<leader>jt', json_to_ts.convert, 'Convert JSON to TS')
+	end,
+}
