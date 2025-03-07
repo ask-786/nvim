beb08793e3cf20b959ec846780ba5a4a295548de
Ahmad bin Marakkar <ahmadshabeerk786@gmail.com>
2025-03-07 10:55:52 +0530

    slkdfjasl;k

--- lua/lazyconfig/plugins/init.lua
+++ lua/lazyconfig/plugins/init.lua
@@ -6,15 +6,6 @@ return {
 	{ 'echasnovski/mini.ai', version = '*', opts = {} },
 	{ 'ask-786/time-calculator.nvim', opts = {} },
 	{ 'ask-786/Comment.nvim', event = 'BufEnter', opts = {} },
-	{
-		'windwp/nvim-autopairs',
-		event = 'InsertEnter',
-		opts = {},
-	},
-	{
-		'kylechui/nvim-surround',
-		version = '*',
-		event = 'VeryLazy',
-		opts = {},
-	},
+	{ 'windwp/nvim-autopairs', event = 'InsertEnter', opts = {} },
+	{ 'kylechui/nvim-surround', version = '*', event = 'VeryLazy', opts = {} },
 }
