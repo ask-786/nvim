bfd025059b8379d795f9ab7a12bfb5d53975483b
Ahmad bin Marakkar <ahmadshabeerk786@gmail.com>
2025-03-14 14:32:08 +0530

    feat: mini adhi prasaram

--- lua/lazyconfig/plugins/init.lua
+++ lua/lazyconfig/plugins/init.lua
@@ -3,10 +3,7 @@ return {
 	{ 'nvim-tree/nvim-web-devicons' },
 	{ 'LunarVim/bigfile.nvim' },
 	{ 'tpope/vim-sleuth' },
-	{ 'echasnovski/mini.ai', version = '*', opts = {} },
 	{ 'ask-786/time-calculator.nvim', opts = {} },
 	{ 'ask-786/Comment.nvim', event = 'BufEnter', opts = {} },
-	{ 'windwp/nvim-autopairs', event = 'InsertEnter', opts = {} },
-	{ 'kylechui/nvim-surround', version = '*', event = 'VeryLazy', opts = {} },
 	{ 'ask-786/emojis.nvim', opts = {} },
 }
---
+++ lua/lazyconfig/plugins/mini.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		'echasnovski/mini.ai',
+		version = '*',
+		opts = {},
+		event = 'InsertEnter',
+	},
+	{
+		'echasnovski/mini.surround',
+		version = '*',
+		opts = {},
+		event = 'InsertEnter',
+	},
+	{
+		'echasnovski/mini.pairs',
+		version = '*',
+		opts = {},
+		event = 'InsertEnter',
+	},
+}
