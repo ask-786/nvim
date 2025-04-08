99dd3070a67dcae405a3fe369d5f9912868b9e44
Ahmad Shabeer K <ahmadshabeerk786@gmail.com>
2025-04-08 18:46:17 +0530

    feat: snacks

--- lua/lazyconfig/plugins/init.lua
+++ lua/lazyconfig/plugins/init.lua
@@ -1,7 +1,6 @@
 return {
 	{ 'nvim-lua/plenary.nvim' },
 	{ 'nvim-tree/nvim-web-devicons' },
-	{ 'LunarVim/bigfile.nvim' },
 	{ 'tpope/vim-sleuth' },
 	{ 'ask-786/time-calculator.nvim', opts = {} },
 	{ 'ask-786/Comment.nvim', event = 'BufEnter', opts = {} },
