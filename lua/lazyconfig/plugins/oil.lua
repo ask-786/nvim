4a4f8bc92bfaaff8cef8f4d4c0b39f1ccb165152
Ahmad Shabeer K <ahmadshabeerk786@gmail.com>
2025-04-11 12:06:56 +0530

    updates in oil

--- lua/lazyconfig/plugins/oil.lua
+++ lua/lazyconfig/plugins/oil.lua
@@ -1,9 +1,23 @@
+_G.get_oil_winbar = function()
+	local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+	local dir = require('oil').get_current_dir(bufnr)
+	if dir then
+		return vim.fn.fnamemodify(dir, ':~')
+	else
+		-- If there is no current directory (e.g. over ssh), just show the buffer name
+		return vim.api.nvim_buf_get_name(0)
+	end
+end
+
 return {
 	'stevearc/oil.nvim',
 	---@module 'oil'
 	---@type oil.SetupOpts
 	opts = {
-		columns = { 'icon', 'size' },
+		view_options = {
+			show_hidden = true,
+			winbar = '%!v:lua.get_oil_winbar()',
+		},
 	},
 	dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
 	lazy = false,
