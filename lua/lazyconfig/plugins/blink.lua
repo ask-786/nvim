4d6c437609e3d3b13229f7816035987e0113a4bc
Ahmad Shabeer K <ahmadshabeerk786@gmail.com>
2025-05-07 10:23:32 +0530

    chore: disable dadbod

--- lua/lazyconfig/plugins/blink.lua
+++ lua/lazyconfig/plugins/blink.lua
@@ -18,12 +18,11 @@ local opts = {
 	sources = {
 		default = { 'lsp', 'path', 'snippets', 'buffer' },
 		per_filetype = {
-			sql = { 'snippets', 'dadbod', 'buffer' },
 			lua = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
 			gitcommit = { 'snippets', 'buffer' },
 		},
 		providers = {
-			dadbod = { name = 'Dadbod', module = 'vim_dadbod_completion.blink' },
+			-- dadbod = { name = 'Dadbod', module = 'vim_dadbod_completion.blink' },
 			lazydev = {
 				name = 'LazyDev',
 				module = 'lazydev.integrations.blink',
